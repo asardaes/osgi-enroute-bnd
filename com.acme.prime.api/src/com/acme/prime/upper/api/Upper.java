c8dae84bd37243fbb7ba9d4cd9dc09529052c3ab
asardaes <alexis.sarda@gmail.com>
2017-03-18 17:18:36 +0100

    Complete quick start tutorial

---
+++ com.acme.prime.api/src/com/acme/prime/upper/api/Upper.java
@@ -0,0 +1,13 @@
+package com.acme.prime.upper.api;
+
+/**
+ * 
+ */
+public interface Upper {
+
+	/**
+	 * Change the input string to upper case
+	 */
+	public abstract String upper(String input);
+
+}
